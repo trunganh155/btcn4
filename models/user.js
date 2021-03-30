65531bb9fc3beb3d495ca9335b421fa92b1f3902
= <=>
2021-03-30 20:27:23 +0700

    make it better

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const users = [{
     id: 1,
     username: '18600022',
     password: '$2b$10$nnxdsq.NeimA2vJffmu32eE1Vz6Whz8dLw2zKiaHBRFY9l0MNUAwe',
+    //password: 'kocopass',
 }];
 
 exports.findByUsername = function (username) {
